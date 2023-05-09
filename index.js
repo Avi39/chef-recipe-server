40e7b8d65f93850afd276973866dc1be06c34696
Avi <avishekuser@gmai.com>
2023-05-10 02:50:18 +0600

    data loaded

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
+const chefs = require('./data/chief.json');
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+app.get('/chefs',(req,res)=>{
+    res.send(chefs);
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
