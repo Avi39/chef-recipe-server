ac621f32f6b45d15dcdbbfa02eeb34ffbb7d65ca
Avi <avishekuser@gmai.com>
2023-05-10 03:13:26 +0600

    cors update

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express');
+var cors = require('cors')
 const app = express();
 const port = process.env.PORT || 5000;
 const chefs = require('./data/chief.json');
+
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
